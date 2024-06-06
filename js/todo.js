0774521454ac6097014cbc93e71e6c458e1571d1
723kb <pp5_vb.swd723@icloud.com>
2024-06-06 18:07:39 +0900

    フッター修正

--- js/todo.js
+++ js/todo.js
@@ -77,11 +77,3 @@ $(document).on('click', '.complete', function () {
   // ページロード時に保存されたToDoリストを読み込む
   loadTodos();
 });
-
-$(function(){
-  //フッターを最下部に固定
-      var $footer = $('#footer');
-      if(window.innerHeight > $footer.offset().top + $footer.outerHeight() ) {
-          $footer.attr({'style': 'position:fixed; top:' + (window.innerHeight - $footer.outerHeight()) + 'px;' });
-      }
-  })
