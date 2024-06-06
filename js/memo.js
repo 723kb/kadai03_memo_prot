4791e70efd442bc3375d5331b4d53e5d58a141db
723kb <pp5_vb.swd723@icloud.com>
2024-06-06 17:51:37 +0900

    フッターを最下部に固定

--- js/memo.js
+++ js/memo.js
@@ -140,3 +140,11 @@ for (let i = 0; i < localStorage.length; i++) {
   }
 }
 
+$(function(){
+  //フッターを最下部に固定
+      var $footer = $('#footer');
+      if(window.innerHeight > $footer.offset().top + $footer.outerHeight() ) {
+          $footer.attr({'style': 'position:fixed; top:' + (window.innerHeight - $footer.outerHeight()) + 'px;' });
+      }
+  })
+
